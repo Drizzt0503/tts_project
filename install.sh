f5ef2808b35a7e38425d3a98b2ad2c7682a40232
YHChen <drizztmon@gmail.com>
2024-02-21 07:20:35 +0000

    add install.sh and requirements.txt

---
+++ install.sh
@@ -0,0 +1,11 @@
+apt-get -y update
+apt-get -y upgrade
+apt-get -y install ffmpeg
+python -m pip install --upgrade pip
+pip uninstall Cython -y
+pip install -r requirements.txt
+cp /database/prosody_bert_model/ ./models/ -r
+cp /database/tts_ecapa_exp/* ./ecapa/exps/ -r
+cd vits_core/monotonic_align
+python setup.py build_ext --inplace
+
