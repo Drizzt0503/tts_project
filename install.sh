eae6271ab237a7c8dffb9ee3b963f810c2a9705b
YHChen <drizztmon@gmail.com>
2024-02-21 07:30:12 +0000

    modified install.sh to NAS dir

--- install.sh
+++ install.sh
@@ -4,8 +4,8 @@ apt-get -y install ffmpeg
 python -m pip install --upgrade pip
 pip uninstall Cython -y
 pip install -r requirements.txt
-cp /database/prosody_bert_model/ ./models/ -r
-cp /database/tts_ecapa_exp/* ./ecapa/exps/ -r
+cp /mnt/share/prosody_bert_model/ ./models/ -r
+cp /mnt/share/tts_ecapa_exp/* ./ecapa/exps/ -r
 cd vits_core/monotonic_align
 python setup.py build_ext --inplace
 
